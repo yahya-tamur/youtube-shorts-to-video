771c9045cc0e48b81c42977393e541df79fcc129
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-07-30 15:07:46 -0700

    no longer redirects youtube.com/<channel>/shorts

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const cr = () => {
-  if(window.location.href.indexOf('shorts') != -1) {
+  let s = window.location.href;
+  //string 'shorts' is in the url and not at the end,
+  //like on a channel's shorts page.
+  if(s.indexOf('shorts') != -1 && s.length - s.indexOf('shorts') > 7) {
     window.location.replace(window.location.href.replace('shorts/', 'watch?v='));
   }
 };
